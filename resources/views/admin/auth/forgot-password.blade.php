e52109b91c93710cc1f3360d68bcd21bdd2ab546
unknown <bruno@3in1.com.br>
2025-06-01 11:07:19 -0300

    feat: View para editar perfil do administrador

--- resources/views/admin/auth/forgot-password.blade.php
+++ resources/views/admin/auth/forgot-password.blade.php
@@ -1,31 +1,3 @@
-{{--
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
---}}
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -63,7 +35,6 @@
             <div class="login-brand">
               <img src="{{ asset('backend/assets/img/logobigodi.svg') }}" alt="biGodi - 3in1.com.br" width="100" class="shadow-light" style="width: 90%; height: auto; box-shadow: none;" >
             </div>
-
             <div class="card card-primary">
               <div class="card-header"><h4>Recuperar Acesso Administrativo</h4></div>
                 <br>
@@ -81,9 +52,6 @@
                         <code>{{ $errors->first('email') }}</code>
                     @endif
                   </div>
-
-
-
                   <div class="form-group">
                     <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4" style="background:#0040e0ff ; color: #fff">
                       Enviar link de recuperação
--- routes/web/admin.php
+++ routes/web/admin.php
@@ -1,9 +1,18 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 //rota admin
-Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])
+Route::get('admin/dashboard', [AdminController::class, 'dashboard'])
 ->middleware(['auth', 'admin'])
 ->name('admin.dashboard');
+
+//rota perfil admin
+Route::get('admin/profile', [ProfileController::class, 'index'])
+->name('admin.profile');
+
+//rota editar perfil admin
+Route::post('admin/profile/update', [ProfileController::class, 'update'])
+->name('admin.profile.update');
